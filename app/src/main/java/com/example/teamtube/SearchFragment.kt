54978b5482aaf5c42fd469849e7dee7c58194df1
junseon <sojh8908@naver.com>
2023-09-26 10:02:27 +0900

    [Design/Feat] SearchFragment UI 구현, 버튼 이벤트 추가

--- app/src/main/java/com/example/teamtube/SearchFragment.kt
+++ app/src/main/java/com/example/teamtube/SearchFragment.kt
@@ -12,8 +12,10 @@ class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -22,6 +24,66 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        val btnHealing = binding.sfBtnHealing
+        val btnAsmr = binding.sfBtnAsmr
+        val btnGame = binding.sfBtnGame
+        val btnMovie = binding.sfBtnMovie
+
+        var isHealingSelected = false
+        var isAsmrSelected = false
+        var isGameSelected = false
+        var isMovieSelected = false
+
+        btnHealing.setOnClickListener {
+            isHealingSelected = !isHealingSelected
+            btnHealing.isSelected = isHealingSelected
+            isAsmrSelected = false
+            btnAsmr.isSelected = false
+            isGameSelected = false
+            btnGame.isSelected = false
+            isMovieSelected = false
+            btnMovie.isSelected = false
+        }
+
+        btnAsmr.setOnClickListener {
+            isAsmrSelected = !isAsmrSelected
+            btnAsmr.isSelected = isAsmrSelected
+            isHealingSelected = false
+            btnHealing.isSelected = false
+            isGameSelected = false
+            btnGame.isSelected = false
+            isMovieSelected = false
+            btnMovie.isSelected = false
+        }
+
+        btnGame.setOnClickListener {
+            isGameSelected =!isGameSelected
+            btnGame.isSelected = isGameSelected
+            isHealingSelected = false
+            btnHealing.isSelected = false
+            isAsmrSelected = false
+            btnAsmr.isSelected = false
+            isMovieSelected = false
+            btnMovie.isSelected = false
+        }
+
+        btnMovie.setOnClickListener {
+            isMovieSelected = !isMovieSelected
+            btnMovie.isSelected = isMovieSelected
+            isHealingSelected = false
+            btnHealing.isSelected = false
+            isAsmrSelected = false
+            btnAsmr.isSelected = false
+            isGameSelected = false
+            btnGame.isSelected = false
+        }
         return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
