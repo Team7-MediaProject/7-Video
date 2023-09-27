5b7462f29b5f628dffcbf680132f4fabb4cd0490
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-27 11:22:43 +0900

    [FIX] : netWorkClient 수정

--- app/src/main/java/com/example/teamtube/Retrofit/VideoNetworkClient.kt
+++ app/src/main/java/com/example/teamtube/Retrofit/VideoNetworkClient.kt
@@ -3,19 +3,15 @@ package com.example.teamtube.Retrofit
 import com.example.teamtube.Constrant.Constrants
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.net.NetworkInterface
 
 object VideoNetworkClient {
+    val apiService: NetworkInterface
+        get() = retrofit.create(NetworkInterface::class.java)
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(Constrants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-
-    // 인기있는 영상 retrofit
-    val popularService = retrofit.create(PopularInterface::class.java)
-    // Category-Channel retrofit
-    val channelService = retrofit.create(ChannelInterface::class.java)
-    // Category-Video retrofit
-    val categoryVideoService = retrofit.create(VideoInterface::class.java)
-    // VideoImage retrofit - 썸네일
-    val VideoService = retrofit.create(VideoImageInterface::class.java)
 }
