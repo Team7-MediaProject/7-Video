c9f3b5c697e620447318c224425298845e81afa8
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-27 11:21:27 +0900

    [FIX] : Interface 수정

--- app/src/main/java/com/example/teamtube/Retrofit/NetworkInterface.kt
+++ app/src/main/java/com/example/teamtube/Retrofit/NetworkInterface.kt
@@ -5,28 +5,27 @@ import com.example.teamtube.Retrofit.Model.Video
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// MostPopular interface
-interface PopularInterface {
-}
-
-// Category-Channel interface
-interface ChannelInterface {
-}
-
-// Category-Video interface
 interface VideoInterface {
-    @GET("videoCategories")
-    suspend fun getCategoryVideoInfo(
-        //@QueryMap param : HashMap<String, String>
-        @Query("part") part: String,
-        @Query("regionCode") regionCode: String,
-        @Query("key") apiKey: String
-    ) : Root
-}
 
-interface VideoImageInterface {
+/*    fun getChannelInfo(
+
+    )*/
+
+    /*fun getPopularInfo(
+
+    )*/
+
+    @GET("videoCategories")
+    fun getCategoryVideoInfo (
+            //@QueryMap param : HashMap<String, String>
+            @Query("part") part: String,
+            @Query("regionCode") regionCode: String,
+            @Query("key") apiKey: String
+    ) : Root
+
     @GET("videos")
-    suspend fun getVideoInfo(
+
+    fun getVideoInfo (
         //@QueryMap param : HashMap<String, String>
         @Query("part") part : String,
         @Query("chart") chart : String,
