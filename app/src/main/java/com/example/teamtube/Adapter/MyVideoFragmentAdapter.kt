88c01f74ec0033a70016a64719e256614bd9bc28
gusdk78951 <139096786+gusdk78951@users.noreply.github.com>
2023-10-05 02:04:47 +0900

    [FEAT] 좋아요 리스트에 SharedPreference 적용

--- app/src/main/java/com/example/teamtube/Adapter/MyVideoFragmentAdapter.kt
+++ app/src/main/java/com/example/teamtube/Adapter/MyVideoFragmentAdapter.kt
@@ -10,7 +10,6 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.teamtube.Fragment.MyVideoFragment
 import com.example.teamtube.MainActivity
 import com.example.teamtube.Model.ChannelModel
 import com.example.teamtube.databinding.CategoryChannelItemBinding
