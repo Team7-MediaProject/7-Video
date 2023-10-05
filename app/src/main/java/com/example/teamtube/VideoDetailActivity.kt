383b85c9f01d295518f953277f4f2cb4c86d4bc2
junseon <sojh8908@naver.com>
2023-10-05 15:27:28 +0900

    Design/Feature 유튜브 플레이어 화면 크기 조절.  유튜브 플레이어 기능 추가

--- app/src/main/java/com/example/teamtube/VideoDetailActivity.kt
+++ app/src/main/java/com/example/teamtube/VideoDetailActivity.kt
@@ -5,16 +5,46 @@ import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.teamtube.Model.HomeitemModel
 import com.example.teamtube.databinding.ActivityVideoDetailBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class VideoDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityVideoDetailBinding
+    private lateinit var youTubePlayerView: YouTubePlayerView
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityVideoDetailBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var detailList = intent.getParcelableExtra<HomeitemModel>("Data")
+        youTubePlayerView = binding.detailView
+        lifecycle.addObserver(youTubePlayerView)
+
+        val detailList = intent.getParcelableExtra<HomeitemModel>("Data")
+
+        detailList?.id?.let { videoId ->
+            youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener(){
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    super.onReady(youTubePlayer)
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+
+                override fun onStateChange(
+                    youTubePlayer: YouTubePlayer,
+                    state: PlayerConstants.PlayerState
+                ) {
+                    super.onStateChange(youTubePlayer, state)
+                    if(state == PlayerConstants.PlayerState.ENDED) {
+                        finish()
+                    }
+                }
+            })
+        }
+        binding.videoTitle.text = detailList?.title
+        binding.detailInfo.text = detailList?.description
+
 
 //        detailList?.id?.let { imageUrl ->
 //            Glide.with(this)
